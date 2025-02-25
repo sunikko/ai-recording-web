25d9af5a78457f9c1ee1cbd724c23ae12ffdf715
Soy <soyeon.won@oxbridgeuk.com>
2025-02-25 02:22:46 +0000

    recording button : MaterialIcons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div className="h-screen bg-[#F6F6F9]"></div>;
+  return (
+    <div className="h-screen bg-[#F6F6F9] relative">
+      <button className="bg-black py-[10px] px-[18px] rounded-[25px] flex items-center absolute bottom-[29px] right-[16px]">
+        <span className="material-icons text-white text-[24px]">mic</span>
+        <span className=" text-white text-[12px] ml-3">녹음하기</span>
+      </button>
+    </div>
+  );
 };
 
 export default Home;
