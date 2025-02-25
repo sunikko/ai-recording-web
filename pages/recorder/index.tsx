42b78496dbb4cf91958b35731fad6d1d9e3310be
Soy <soyeon.won@oxbridgeuk.com>
2025-02-25 02:46:14 +0000

    record page 1: useRouter

---
+++ pages/recorder/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+const Recorder = () => {
+  return (
+    <div className="h-screen bg-white flex flex-col">
+      <Header title="Recording" />
+    </div>
+  );
+};
+
+export default Recorder;
