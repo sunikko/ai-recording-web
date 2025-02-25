68ee7f1dbe3bf251fc474084b298bc3a813fef12
Soy <soyeon.won@oxbridgeuk.com>
2025-02-25 02:02:34 +0000

    basic set up: for web-app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="max-w-screen-sm mx-auto">
         <Main />
         <NextScript />
       </body>
